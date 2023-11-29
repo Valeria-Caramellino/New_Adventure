0a5e15892bf3b500dae1185f39600f7c8c645048
valeria caramellino <valeriacaramellino@gmail.com>
2023-11-29 09:56:06 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style/style.scss'
 import App from './App.vue'
+
+//Import Bootstrap
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
